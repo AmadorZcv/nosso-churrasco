648505de3848a86e7c884fcb1f5d2b1bffe0ae4f
AmadorZcv <lucasamadorzcv@gmail.com>
2018-06-25 19:06:52 -0300

    Fetch correto do banco de grupos

--- Web/tabs/grupo/grupo.php
+++ Web/tabs/grupo/grupo.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+$userid = $_SESSION['userid'];
+
+?>
+
 <html>
 <head>
 
@@ -9,7 +15,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
     <link rel="stylesheet" href="index.css">
     </head>
-    
+
     <body>
     <a class="waves-effect waves-orange btn white lighten-3 black-text" style="left: 10; top: 10" href="../index.php">Voltar</a>
     <br>
@@ -20,42 +26,44 @@
                         <div class="card-content s8 black-text">
                         <span class="card-title black-text" align="center"><b>Grupos:</b></span>
                         <ul class="collection" style="position:absolute; width:815; height:255; z-index:1; overflow: auto">
-                        <?php
-                        require_once '../../config.php';
-$sql = "SELECT * FROM churrasco";
-$result = mysqli_query($link,"SELECT * FROM churrasco");
-        if ($stmt = mysqli_prepare($link, $sql)) {
-            // Bind variables to the prepared statement as parameters
-            // Set parameters
-            // Attempt to execute the prepared statement
-            if (mysqli_stmt_execute($stmt)) {
-                // Store result
-                $result2 =mysqli_stmt_get_result($stmt);
-                mysqli_fetch_array($result2,MYSQLI_NUM);
-                $collection="collection-item avatar";
-                $materialIcons = "material-icons circle";
-                $grupoImagem = "grupo-imagem";
-                
-                while($row = mysqli_fetch_array($result))
-                    {
-                       $churrasName = $row["churras_name"];
-                       echo "<li class='$collection'>";
-                       echo  "<i class='$materialIcons'>";
-                       echo  "<div class='$grupoImagem' align='center'>";
-                       echo  " <img src='../../assets/img/simboloPorcao.png' alt='imagem do grupo' style='width: 50px; height: 50px'>";
-                       echo"</div>";
-                       echo "</i>";
-                       echo "<a class='title' href='../detalhes_do_grupo/grupo_detalhes.php'>'$churrasName'</a>";
-                       echo "<p>PAGOU A COLETA";
-                       echo "</p>";
-                       echo "</li>";
-                    }
-            } else {
-                echo "Oops! Something went wrong. Please try again later.";
-            }
+            <?php
+require_once '../../config.php';
+$sql = "SELECT B.churras_name, B.churras_datetime, B.churras_ds_adress, B.churras_image
+                FROM user_has_churrasco A
+                INNER JOIN churrasco B ON B.id = A.churrasco_id
+                WHERE A.user_id = ?";
+if ($stmt = mysqli_prepare($link, $sql)) {
+    // Bind variables to the prepared statement as parameters
+    mysqli_stmt_bind_param($stmt, 'i', $userid);
+    // Set parameters
+    // Attempt to execute the prepared statement
+    if (mysqli_stmt_execute($stmt)) {
+        // Store result
+        $result = mysqli_stmt_get_result($stmt);
+        $collection = "collection-item avatar";
+        $materialIcons = "material-icons circle";
+        $grupoImagem = "grupo-imagem";
+        while ($row = mysqli_fetch_array($result)) {
+            $churrasName = $row["churras_name"];
+            $churrasDatetime = $row["churras_datetime"];
+            $churrasAdress = $row["churras_ds_adress"];
+            $churrasImage = $row["churras_image"];
+            echo "<li class='$collection'>";
+            echo "<i class='$materialIcons'>";
+            echo "<div class='$grupoImagem' align='center'>";
+            echo " <img src='../../assets/img/simboloPorcao.png' alt='imagem do grupo' style='width: 50px; height: 50px'>";
+            echo "</div>";
+            echo "</i>";
+            echo "<a class='title' href='../detalhes_do_grupo/grupo_detalhes.php'>'$churrasName'</a>";
+            echo "<p>'$churrasDatetime'</p>";
+            echo "<p>PAGOU A COLETA";
+            echo "</p>";
+            echo "</li>";
         }
-
-
+    } else {
+        echo "Oops! Something went wrong. Please try again later.";
+    }
+}
 
 mysqli_close($link);
 ?>
@@ -70,5 +78,5 @@ mysqli_close($link);
                 </div>
             </div>
         </div>
-    </body> 
+    </body>
 </html>
