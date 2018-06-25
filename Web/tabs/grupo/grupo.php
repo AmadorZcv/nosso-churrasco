39f651753730b8162c5479d5561c44da92f8a9c7
AmadorZcv <lucasamadorzcv@gmail.com>
2018-06-25 15:55:50 -0300

    Tela de grupo pegando todos os grupos do banco

--- Web/tabs/grupo/grupo.php
+++ Web/tabs/grupo/grupo.php
@@ -3,6 +3,7 @@
 
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+    <link rel="stylesheet" href="../../css/materialize.min.css">
     <link rel="stylesheet" type="text/css" href="grupo.css">
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
@@ -16,58 +17,54 @@
                 <div class="row">
                     <div class="col s11 ">
                     <div class="card white">
-                        <div class="card-content s12 black-text">
+                        <div class="card-content s8 black-text">
                         <span class="card-title black-text" align="center"><b>Grupos:</b></span>
-                        
                         <ul class="collection" style="position:absolute; width:815; height:255; z-index:1; overflow: auto">
-                            <li class="collection-item avatar">
-                            <i class="material-icons circle">
-                            <div class="grupo-imagem" align="center">
-                                <img src="../../assets/img/simboloPorcao.png" alt="imagem do grupo" style="width: 50px; height: 50px">
-                            </div>
-                            </i>
-                            <a class="title" href="../detalhes_do_grupo/grupo_detalhes.php">NOME</a>
-                            <p>PAGOU A COLETA
-                            </p>
-                            </li>
-                               
-                            <li class="collection-item avatar">
-                            <i class="material-icons circle">
-                            <div class="grupo-imagem" align="center">
-                                <img src="../../assets/img/simboloPorcao.png" alt="imagem do grupo" style="width: 50px; height: 50px">
-                            </div>
-                            </i>
-                            <a class="title" href="../detalhes_do_grupo/grupo_detalhes.php">NOME</a>
-                            <p>NÃO PAGOU A COLETA
-                            </p>
-                            </li>
-                                
-                            <li class="collection-item avatar">
-                            <i class="material-icons circle">
-                            <div class="grupo-imagem" align="center">
-                                <img src="../../assets/img/simboloPorcao.png" alt="imagem do grupo" style="width: 50px; height: 50px">
-                            </div>
-                            </i>
-                            <a class="title" href="../detalhes_do_grupo/grupo_detalhes.php">NOME</a>
-                            <p>PAGOU A COLETA
-                            </p>
-                            </li>
+                        <?php
+                        require_once '../../config.php';
+$sql = "SELECT * FROM churrasco";
+$result = mysqli_query($link,"SELECT * FROM churrasco");
+        if ($stmt = mysqli_prepare($link, $sql)) {
+            // Bind variables to the prepared statement as parameters
+            // Set parameters
+            // Attempt to execute the prepared statement
+            if (mysqli_stmt_execute($stmt)) {
+                // Store result
+                $result2 =mysqli_stmt_get_result($stmt);
+                mysqli_fetch_array($result2,MYSQLI_NUM);
+                $collection="collection-item avatar";
+                $materialIcons = "material-icons circle";
+                $grupoImagem = "grupo-imagem";
+                
+                while($row = mysqli_fetch_array($result))
+                    {
+                       $churrasName = $row["churras_name"];
+                       echo "<li class='$collection'>";
+                       echo  "<i class='$materialIcons'>";
+                       echo  "<div class='$grupoImagem' align='center'>";
+                       echo  " <img src='../../assets/img/simboloPorcao.png' alt='imagem do grupo' style='width: 50px; height: 50px'>";
+                       echo"</div>";
+                       echo "</i>";
+                       echo "<a class='title' href='../detalhes_do_grupo/grupo_detalhes.php'>'$churrasName'</a>";
+                       echo "<p>PAGOU A COLETA";
+                       echo "</p>";
+                       echo "</li>";
+                    }
+            } else {
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
 
-                            <li class="collection-item avatar">
-                            <i class="material-icons circle">
-                            <div class="grupo-imagem" align="center">
-                                <img src="../../assets/img/simboloPorcao.png" alt="imagem do grupo" style="width: 50px; height: 50px">
-                            </div>
-                            </i>
-                            <a class="title" href="../detalhes_do_grupo/grupo_detalhes.php">NOME</a>
-                            <p>PAGOU A COLETA
-                            </p>
-                            </li>
+
+
+mysqli_close($link);
+?>
+
                         </ul>
                     </div>
                     <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
                     <div class="card-action black-text">
-                        <a href="../criar_grupo/index.html">Criar Novo Grupo.</a>
+                        <a href="../criar_grupo/index.php">Criar Novo Grupo.</a>
                     </div>
                     </div>
                 </div>
